db0802e4cf2bd8162f83f439dae599bbd851eda7
kuboyoichiro <kubo_yoichiro_xa@cyberagent.co.jp>
2021-02-18 17:53:23 +0900

    feat: 簡単なTODO with vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,66 @@
-import { createStore } from "vuex";
+import { InjectionKey } from 'vue'
+import { createStore, useStore as baseUseStore, Store, ActionContext } from "vuex";
 
-export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+type TodoItem = {
+  title: string;
+  isCompleted: boolean;
+};
+export interface State {
+  todoList: Array<TodoItem>;
+};
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
+  state: {
+    todoList: [],
+  },
+  getters: {
+    uncompletedTodoList: (state) => state.todoList.filter(item => !item.isCompleted),
+  },
+  mutations: {
+    addTodo(state: State, payload: string): void {
+      state.todoList.push({
+        title: payload,
+        isCompleted: false,
+      });
+      return;
+    },
+    clearTodo(state: State): void {
+      state.todoList.splice(-state.todoList.length);
+      return;
+    },
+    removeTodo(state: State, index: number): void {
+      state.todoList.splice(index, 1);
+      return;
+    },
+    toggleTodoCompleteState(state: State, index: number): void {
+      const target = state.todoList[index];
+      state.todoList[index].isCompleted = !!target.isCompleted;
+      return;
+    }
+  },
+  actions: {
+    addTodo({ commit }: ActionContext<State, State>, payload: string): void {
+      commit('addTodo', payload);
+      return;
+    },
+    clearTodo({ commit }: ActionContext<State, State>): void {
+      commit('clearTodo');
+      return;
+    },
+    removeTodo({ commit }: ActionContext<State, State>, payload: number): void {
+      commit('removeTodo', payload);
+      return;
+    },
+    toggleTodoCompleteState({ commit }: ActionContext<State, State>, payload: number): void {
+      commit('toggleTodoCompleteState', payload);
+      return;
+    },
+  }
+})
+
+// define your own `useStore` composition function
+export function useStore () {
+  return baseUseStore(key)
+}
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="home">
+  <div class="todo">
     <p>todo</p>
+    <button @click="addTodo">add</button>
+    <button @click="clearTodo">clear</button>
+    <ul>
+      <li v-for="(todo, index) in state.todoList" :key="todo.title">
+        <span>{{ todo.title }}</span>
+        <input type="checkbox" :checked="todo.isCompleted" @change="toggleTodoCompleteState(index)">
+        <button @click="removeTodo(index)">remove</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
+import { useStore } from "vuex";
+import { State } from '../store';
+
+type ComponentState = State;
 
 export default defineComponent({
   name: "Todo",
+  setup() {
+    const store = useStore();
+    const state: ComponentState = reactive<ComponentState>({
+        todoList: store.state.todoList,
+    });
+
+    const addTodo = () => {
+      const rand = Math.random();
+      store.dispatch('addTodo', `hogefuga${rand}`);
+      return;
+    };
+
+    const clearTodo = () => {
+      store.dispatch('clearTodo');
+      return;
+    }
+
+    const removeTodo = (index: number) => {
+      store.dispatch('removeTodo', index);
+      return;
+    }
+
+    const toggleTodoCompleteState = (index: number) => {
+      store.dispatch('toggleTodoCompleteState', index);
+      return;
+    }
+
+    return {
+      state,
+      addTodo,
+      clearTodo,
+      removeTodo,
+      toggleTodoCompleteState,
+    };
+  },
 });
 </script>
